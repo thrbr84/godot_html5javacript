9736e94fbd0942462b8a005ebafb386516e00776
Thiago Bruno <thiago.bruno@vogcalgaryappdeveloper.com>
2021-11-04 21:12:31 -0600

    New examples

---
+++ Example/funcoes.js
@@ -0,0 +1,69 @@
+var n = 1;
+
+const functions = {
+    "restartNumber": (num = 1) => {
+        n = num;
+        window.localStorage.setItem("number", n);
+        functions["updateLabel"]();
+    },
+    "login": () => {
+        window.localStorage.setItem("logged", "1");
+    },
+    "logout": () => {
+        window.localStorage.removeItem("logged");
+    },
+    "increaseNumber": () => {
+        n++;
+        window.localStorage.setItem("number", n);
+        functions["updateLabel"]();
+    },
+    "decreaseNumber": () => {
+        n = n <= 1 ? 1 : n-1
+        window.localStorage.setItem("number", n);
+        functions["updateLabel"]();
+    },
+    "updateLabel": () => {
+        if ($("#labelNumber").length > 0)
+            $("#labelNumber").text(n);
+    },
+    "api_get_user": async(id) => {
+        return await fetch('https://jsonplaceholder.typicode.com/todos/' + id)
+            .then(response => response.json());
+    }
+}
+
+// If you use the javascript in another format like function expressions above, you need to convert the function like below
+// The Godot can call the functions in this format
+function restartNumber(num = 1) { 
+    return functions["restartNumber"](num) 
+}
+
+function login() { 
+    return functions["login"]() 
+}
+
+function logout() { 
+    return functions["logout"]() 
+}
+
+function increaseNumber() { 
+    return functions["increaseNumber"]() 
+}
+
+function decreaseNumber() { 
+    return functions["decreaseNumber"]() 
+}
+
+async function api_get_user(id = 1) { 
+    return await functions["api_get_user"](id).then(x => {
+        window.localStorage.setItem("user", JSON.stringify(x));
+    });
+}
+
+$(document).ready(function(){
+    var _loadNumber = window.localStorage.getItem("number");
+    if (_loadNumber) {
+        n = parseInt(_loadNumber);
+    }
+    functions["updateLabel"]();
+});
--- Godot/scripts/main.gd
+++ Godot/scripts/main.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
+var userDataReady = false
+var _number = 0
+
 func _ready():
 	$Lock.play("locked")
 	$Label.text = "1"
+	$User.text = "..."
 	
 func _on_Timer_timeout():
-	var _number = JavaScript.eval("window.localStorage.getItem('number')")
+	_number = JavaScript.eval("window.localStorage.getItem('number')")
 	if (_number):
 		$Label.text = _number
 
 func _on_btnRestart_pressed():
-	JavaScript.eval("parent.restartNumber()")
+	var restartNum = 1
+	JavaScript.eval(str("parent.restartNumber(", restartNum, ")"))
 
 func _on_btnIncrease_pressed():
 	JavaScript.eval("parent.increaseNumber()")
@@ -23,6 +28,25 @@ func _on_checkLogin_timeout():
 	if (_logged):
 		if (_logged == "1"):
 			$Lock.play("unlocked")
+			
+			# Return user data
+			if (userDataReady == false):
+				userDataReady = true
+				JavaScript.eval(str("parent.api_get_user(", _number, ")"))
+				$CheckUser.start()
 			return
 	
 	$Lock.play("locked")
+	$CheckUser.stop()
+	$User.text = ""
+	userDataReady = false
+
+func _on_CheckUser_timeout():
+	var userData = JavaScript.eval("window.localStorage.getItem('user')")
+	if (userData):
+		$CheckUser.stop()
+		
+		var stringResult: String = userData
+		var jsonParseResult: JSONParseResult = JSON.parse(stringResult)
+		var userJson = jsonParseResult.result
+		$User.text = str(userJson["title"])
